8e49671a22323e75f85a87930f8bc0845b3be261
Oayaou08 <ayaouomar333@gmail.com>
2025-12-25 11:02:19 +0000

    .

--- script.js
+++
@@ -1,9 +0,0 @@
-<script>
-  document.querySelectorAll('a[href^="#"]').forEach(enlace => {
-    enlace.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href'))
-        .scrollIntoView({ behavior: 'smooth' });
-    });
-  });
-</script>
